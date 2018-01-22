15030e95c4dffe7c66d388f8faa3cb159add9bc7
j.urbina.0179@gmail.com <j.urbina.0179@gmail.com>
2018-01-22 16:16:10 +0100

    files

--- src/FileElement/FileElement.js
+++ src/FileElement/FileElement.js
@@ -1,21 +1,117 @@
-var carpeta = new Array();
-var archivo = new Array();
+class File {
+    constructor(name) {
+        this.name = name;
+        this.content = ""
+    }
+     
+    
+    setContent(content) {
+        this.content = content;
+    }
 
-function crearCarpeta(){
-    var x = $("#carpeta").val();
-    carpeta.push(x);
-    $("#carpeta").val('');
-    return carpeta;
+    
 }
 
-function crearArchivo(){
-    var x = $("#archivo").val();
-    archivo.push(x);
-    $("#archivo").val('');
-    return archivo;
+class Folder {
+    constructor(name) {
+        this.name = name;
+        this.files = []
+        this.folders = []
+    }
+    
+    addFile(file) {
+        this.files.push(file);
+    }
+    
+    addFolder(name) {
+        this.folders.push(new Folder(name))
+    }
+    
+    folderExists(folder) {
+        for(let a = 0; a <= this.folders.length; a++) {
+            let folder = this.folders[a];
+            if (folder.name == folder) return true;
+        }
+        
+        return false;
+    }
+    
+    getFolder(folder) {
+        for(let a = 0; a <= this.folders.length; a++) {
+            let folder = this.folders[a];
+            if (folder.name == folder) return folder;
+        }
+        
+    }
 }
-var carpeta = crearCarpeta();
-    var archivo = crearArchivo();
+
+class FileSystem {
+    constructor() {
+        this.files = []
+        this.folders = []
+        
+
+    }
+    
+    addFolder(name) {
+        this.folders.push(new Folder(name));
+    }
+    
+    folderExists(folder) {
+        for(let a = 0; a <= this.folders.length; a++) {
+            let folder = this.folders[a];
+            if (folder.name == folder) return true;
+        }
+        
+        return false;
+    }
+    
+    getFolder(folder) {
+        for(let a = 0; a <= this.folders.length; a++) {
+            let folder = this.folders[a];
+            if (folder.name == folder) return folder;
+        }
+        
+    }
+    
+    addFile(name, content, folder) {
+        let newFile = new File(name);
+        newFile.setContent(content);
+        
+        if (folder && this.folderExists(folder) {
+            let folderObj = this.getFolder(folder);
+            folderObj.addFile(newFile);
+        }
+    }
+
+    addInSubfolder(folders, name, content) {
+        let ref;
+        for(let a = 0; a <= folders.length; a++) {
+            let folderName = folders[a];
+            if (ref) {
+                let ref = ref.getFolder(folderName);
+            } else {
+                let ref = this.getFolder(folderName);
+            }
+        }
+        
+        let newFile = new File(name);
+        newFile.setContent(content);
+        
+        ref.addFile(newFile);
+    }
+
+    //addInSubfolder(['css', ['boostrap']], file);
+}
+
+
+
+
+
+
+
+
+
 function zip(){
     
     
